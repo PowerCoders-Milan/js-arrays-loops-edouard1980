fa1537de5d44180df56b0f7ccdb577863962d4a5
edouard1980 <excellenceorphe@gmail.com>
2021-06-18 13:05:15 +0200

    exo111
    .exit

--- 01-array/README.md
+++ 01-array/README.md
@@ -1,4 +1,5 @@
 # array
 
 1. create an `array` with 5 elements.
+
 2. `console.log()` the 3rd element.
--- 01-array/array.js
+++ 01-array/array.js
@@ -1,4 +1,5 @@
 // define an array with 5 elements
-const array = [];
+const array = ['element1','element2','element3','element4','element5'];
 // console.log() the 3rd element
-console.log();
+
+console.log(array[2]);
