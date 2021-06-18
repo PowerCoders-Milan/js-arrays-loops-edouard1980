e039f71a331a6219eeb21c6b4774ef9b9fc3a64c
edouard1980 <excellenceorphe@gmail.com>
2021-06-18 15:22:16 +0200

    exo2S

--- 02-strings/reverse_string.js
+++ 02-strings/reverse_string.js
@@ -1,1 +1,8 @@
 // read carefully the instructions
+const myString = 'bonjour';
+
+let stringarray=myString.split("");
+stringarray.reverse()
+let string=stringarray.join("")
+
+console.log(string);
